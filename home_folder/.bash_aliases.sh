4e369a571aef082c2313c46c53b020772f49bef6
Sudhanshu Gupta <sudhanshu.gupta@chargepoint.com>
2020-04-15 13:37:23 +0530

    Added chargepoint specific source path aliases
    Signed-off-by: Sudhanshu Gupta <sudhanshu.gupta@chargepoint.com>

--- home_folder/.bash_aliases.sh
+++ home_folder/.bash_aliases.sh
@@ -48,6 +48,7 @@ alias interviewprep='cd $HOME/tmp/01_linux_learning'
 alias cp4k='cd $HOME/cp_src/emb-cphm400-build'
 alias chaps='cd $HOME/cp_src/emb-chaps'
 alias chui='cd $HOME/cp_src/emb-chui'
+alias sitara='cd $HOME/cp_src/emb-sitara-chaps'
 fi
 
 #################################################################
