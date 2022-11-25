3b866f65bd94397e66218f435d913ce76455197d
Sudhanshu <chargepoint@CTIPL154-MacBook-Pro.local>
2022-11-25 17:18:56 +0530

    update .ssh folder with install script and private and public keys
    Signed-off-by: Sudhanshu <chargepoint@CTIPL154-MacBook-Pro.local>

--- home_folder/.ssh/install.sh
+++ home_folder/.ssh/install.sh
@@ -1,2 +1,6 @@
+# copy ssh config
 cp ~/support_package/home_folder/.ssh/config ~/.ssh/config
 chmod 600 ~/.ssh/config
+
+# copy private and public keys
+cp ~/support_package/home_folder/.ssh/id_rsa*  ~/.ssh/
