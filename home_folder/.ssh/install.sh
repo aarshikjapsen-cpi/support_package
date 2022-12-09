4008869005174cca6afc922dfbc72ea7816fef3e
aarshikjapsen-cpi <92792660+aarshikjapsen-cpi@users.noreply.github.com>
2022-12-09 13:44:19 +0530

    Delete home_folder/.ssh directory

--- home_folder/.ssh/install.sh
+++
@@ -1,6 +0,0 @@
-# copy ssh config
-cp ~/support_package/home_folder/.ssh/config ~/.ssh/config
-chmod 600 ~/.ssh/config
-
-# copy private and public keys
-cp ~/support_package/home_folder/.ssh/id_rsa*  ~/.ssh/
